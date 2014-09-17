5fa9d306ab96df2a22c5c2f95879435d35c266a1
Jon Seymour <jon@ninjablocks.com>
2014-09-18 01:05:42 +1000

    Add initialization support.
    The functions selected here are exactly those functions that
    the MinOZW sample application uses to initialise the C++ library
    and start receiving information from the ZWave network.
    
    Each function in api.go is paired with a corresponding C declaration
    in wrapping wrapper.hpp and a C++ implementation in wrapper.cpp.
    
    api.go handles marshalling between Go and C.
    wrapper.cpp handles marhsalling between C and C++.
    
    Missing from the moment is definition of a watcher function which needs
    to reflect notifications generated by the C++ library back through the C layer
    and into Go.
    
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- api.go
+++ api.go
@@ -1,6 +1,14 @@
 package openzwave
 
 //
+// Provides a facade for the C++ API that exposes just enough of the underlying C++
+// API to be useful to implementing the Ninja Zwave driver
+//
+// The functions in this module are responsible for marshalling to and from the C functions
+// declared in wrapper.hpp and wrapper.cpp.
+//
+
+//
 // #cgo LDFLAGS: -lopenzwave -L../go-openzwave/openzwave
 // #cgo CPPFLAGS: -Iopenzwave/cpp/src/platform -Iopenzwave/cpp/src -Iopenzwave/cpp/src/value_classes
 //
@@ -10,13 +18,66 @@ import "C"
 
 import "unsafe"
 
+var LogLevel_Detail int = int(C.LogLevel_Detail)
+var LogLevel_Error int = int(C.LogLevel_Error)
+var LogLevel_Debug int = int(C.LogLevel_Debug)
+var LogLevel_Info int = int(C.LogLevel_Info)
+
 type API struct {
-	available bool // true if a zwave device is available.
+	options C.Options // an opaque reference to C++ Options object
+	manager C.Manager // an opaque reference to C++ Manager opject
 }
 
-func NewAPI(device string) *API {
-	var cDevice *C.char = C.CString(device);
-	defer C.free(unsafe.Pointer(cDevice));
-	var rc C.int = C.init(cDevice);
-	return &API{rc != 0}
+// allocate the control block used to track the state of the API
+func NewAPI() *API {
+	return &API{nil, nil}
+}
+
+// create and stash the C++ Options object
+func (self *API) CreateOptions(configPath string, logPath string) *API {
+	var cConfigPath *C.char = C.CString(configPath)
+	var cLogPath *C.char = C.CString(logPath)
+	defer C.free(unsafe.Pointer(cConfigPath))
+	defer C.free(unsafe.Pointer(cLogPath))
+	self.options = C.createOptions(cConfigPath, cLogPath)
+	return self
+}
+
+// configure the C++ Options object with an integer value
+func (self *API) AddIntOption(option string, value int) *API {
+	var cOption *C.char = C.CString(option)
+	defer C.free(unsafe.Pointer(cOption))
+
+	C.addIntOption(self.options, cOption, C.int(value))
+	return self
+}
+
+// configure the C++ Options object with a boolean value
+func (self *API) AddBoolOption(option string, value bool) *API {
+	var cOption *C.char = C.CString(option)
+	var cBool C.int
+
+	defer C.free(unsafe.Pointer(cOption))
+	if value {
+		cBool = C.TRUE
+	} else {
+		cBool = C.FALSE
+	}
+	C.addBoolOption(self.options, cOption, cBool)
+	return self
+}
+
+// lock the options object and allocate the manager object.
+func (self *API) LockOptions() *API {
+	self.manager = C.lockOptions(self.options)
+	return self
+}
+
+// add a driver.
+func (self *API) AddDriver(device string) *API {
+	var cDevice *C.char = C.CString(device)
+	defer C.free(unsafe.Pointer(cDevice))
+
+	C.addDriver(self.manager, cDevice)
+	return self
 }
--- wrapper.hpp
+++ wrapper.hpp
@@ -1,7 +1,27 @@
+//
+// wrapper.hpp
+//
+// Provides marshalling between C and C++ abstractions. There is typically one C function in this module for each Go function in api.go
+//
+// See wrapper.cpp for additional information.
+//
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-int init(char * device);
+typedef void * Options;
+typedef void * Manager;
+extern int TRUE;
+extern int FALSE;
+extern int LogLevel_Detail;
+extern int LogLevel_Debug;
+extern int LogLevel_Info;
+extern int LogLevel_Error;
+extern Options createOptions(char *, char *);
+extern void addIntOption(Options, char *, int );
+extern void addBoolOption(Options, char *, int);
+extern Manager lockOptions(Options );
+extern void addDriver(Manager , char *);
 #ifdef __cplusplus
 }
 #endif
