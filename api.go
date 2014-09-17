35b67cf3d590848014a55f78285bfc77437f2f43
Jon Seymour <jon.seymour@gmail.com>
2014-09-17 18:34:04 +1000

    Rename api to api.go
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

---
+++ api.go
@@ -0,0 +1,22 @@
+package openzwave
+
+//
+// #cgo LDFLAGS: -lopenzwave 
+// #cgo CPPFLAGS: -Iopenzwave/cpp/src/platform -Iopenzwave/cpp/src -Iopenzwave/cpp/src/value_classes
+//
+// #include <stdlib.h>
+// #include "wrapper.hpp"
+import "C"
+
+import "unsafe"
+
+type API struct {
+	available bool // true if a zwave device is available.
+}
+
+func NewAPI(device string) *API {
+	var cDevice *C.char = C.CString(device);
+	defer C.free(unsafe.Pointer(cDevice));
+	var rc C.int = C.init(cDevice);
+	return &API{rc != 0}
+}
