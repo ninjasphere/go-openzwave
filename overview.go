ba75f163081422b64b285ae19a80af005bab6e54
Jon Seymour <jon@ninjablocks.com>
2014-09-22 08:13:35 +1000

    api.do: clean up documentation.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- overview.go
+++ overview.go
@@ -1,4 +1,30 @@
 //
 // openzwave provides a thin Go wrapper around the openzwave library.
 //
+// The following shows a simple use of the API which will log every notification received.
+//
+//        var loop = func(api openzwave.API) {
+//                fmt.Printf("event loop starts\n")
+//                for {
+//                        select {
+//                        case notification := <-api.Notifications():
+//                                fmt.Printf("notification received - %v", notification)
+//                                api.FreeNotification(notification)
+//                        case quitNow := <-api.QuitSignal():
+//                                _ = quitNow
+//                                fmt.Printf("event loop ends\n")
+//                                return
+//                        }
+//                }
+//        }
+//
+//        os.Exit(openzwave.
+//                BuildAPI("../go-openzwave/openzwave/config", "", "").
+//                AddIntOption("SaveLogLevel", LOG_LEVEL.NONE).
+//                AddIntOption("QueueLogLevel", LOG_LEVEL.NONE).
+//                AddIntOption("DumpTrigger", LOG_LEVEL.NONE).
+//                AddIntOption("PollInterval", 500).
+//                AddBoolOption("IntervalBetweenPolls", true).
+//                AddBoolOption("ValidateValueChanges", true).
+//                Run(loop))
 package openzwave
