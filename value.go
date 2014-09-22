746724b8b0a0dc8dcfe1bd293c520b33b314ebbc
Jon Seymour <jon@ninjablocks.com>
2014-09-22 16:49:37 +1000

    Add value.go as a home for go routines about Value and ValueIDs

---
+++ value.go
@@ -0,0 +1,17 @@
+package openzwave
+
+// #cgo LDFLAGS: -lopenzwave -Lgo/src/github.com/ninjasphere/go-openzwave/openzwave
+// #cgo CPPFLAGS: -Iopenzwave/cpp/src/platform -Iopenzwave/cpp/src -Iopenzwave/cpp/src/value_classes
+//
+// #include "api.h"
+import "C"
+
+
+type Value struct {
+	inC *C.Value
+}
+
+type ValueID struct {
+	inC *C.ValueID
+}
+
